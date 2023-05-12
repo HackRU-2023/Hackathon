181ee6dd9950c68de61f978ff7ded5a2753d7f83
lizashva <lizashva556@gmail.com>
2023-05-12 05:39:15 +0300

    ccc

--- Server/FlaskServer.py
+++ Server/FlaskServer.py
@@ -3,19 +3,19 @@ import json
 from bson import json_util
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
-#from Utils import InitUtil as IU
+# from Utils import InitUtil as IU
 from bson import json_util
-
-
-
 
 from Server.Database.connection import InitMongo
 from Server.Database.dataBase import DataBase
+from Server.Models.agent import Agent
 from Server.Simulator.OpenAISimulator import OpenAISimulator
 from Server.Utils.Voice import Voice
 
 app = Flask(__name__)
 CORS(app)
+emotion = "NATURAL"
+
 
 def get_client_agent_strongs(db):
     agent = db.get_agent("NglT4UH7dzTYD6EEmW64NvzKQZ82")
@@ -25,6 +25,7 @@ def get_client_agent_strongs(db):
     client_personals = client_skills[0]
     client_emotions = client_skills[1]
     return agent_skills, client_personals, client_emotions
+
 
 def load_config_file(config_path):
     # Load the configuration from the JSON file
@@ -37,9 +38,9 @@ def load_config_file(config_path):
 def home():
     return jsonify({'message': 'Hello from Flask server!'})
 
+
 # @app.route('/api/users/<id>', methods=['GET'])
 # def login(id):
-
 
 
 @app.route('/api/data', methods=['GET'])
@@ -58,13 +59,14 @@ def get_skills_to_fill():
 
     return jsonify(skills)
 
- jsonify(agent)
-app.route('/api/get_agent', methods=['GET'])
+
+@app.route('/api/get_agent', methods=['GET'])
 def get_agent():
     agent_id = request.json.get('agent_id')
     agent = db.get_login(agent_id)
     agent = json.loads(json_util.dumps(agent))
     return jsonify(agent)
+
 
 @app.route('/api/skills_template', methods=['GET'])
 def get_skills_template():
@@ -74,10 +76,16 @@ def get_skills_template():
     skills = json.loads(json_util.dumps(skills))
     return jsonify(skills)
 
+
 @app.route('/api/get_review', methods=['GET'])
 def get_review():
     result = simulator.review_simulation(simulation_id)
     return jsonify(result)
+
+
+# @app.route('/api/simulation_finish', methods=['POST'])
+# def post_finish_simulation():
+#     update_sim_finish
 
 @app.route('/api/transcription_exchange', methods=['POST'])
 def post_transcription():
@@ -101,18 +109,55 @@ def post_transcription():
 
 
 # company_description, emotions, personality, call_subject
+@app.route('/api/emotion', methods=['GET'])
+def get_client_emotion():
+    return emotion
+
+
+# company_description, emotions, personality, call_subject
 @app.route('/api/situation_description', methods=['POST'])
 def get_company_description():
     emotions = request.json.get('emotions')
     personality = request.json.get('personality')
     situation_description = request.json.get('situation_description')
-    simulation_id = simulator.start_simulation(config["CompanyInfo"], emotions,
+    simulation_id = simulator.start_simulation(config["A company that provide internet"], emotions,
                                                personality,
                                                situation_description)
 
 
-if __name__ == '__main__':
+@app.route('/api/convertEmotions', methods=['GET'])
+def convert_emotions():
+    agent_id = request.json.get('agent_id')
+    agent = db.get_agent(agent_id)  # type: Agent
+    skills = agent.skills
+    # client_emotions = ["CALM", "ANGRY", "HAPPY", "SURPRISED", "HOPEFUL",
+    #                    "CONFUSED", "DISAPPOINTED", "NATURAL"]
+    # client_qualifies = ["empathy", "patience", "fast talk", "stuttering speech",
+    #                     "weak voice", "monotonous speech", "rude", "stingy",
+    #                     "gentle", "assertive", "manipulative"]
+    c_emotion_scores = {"CALM": 7, "ANGRY": 5, "HAPPY": 2, "SURPRISED": 0, "HOPEFUL": 4,
+                        "CONFUSED": 3, "DISAPPOINTED": 9, "NATURAL": 7}
+    c_qualifies_scores = [int(skills["empathy"]),
+                          int(skills["patience"]),
+                          int(skills["expressiveness"]),
+                          int(skills["emotional management"]),
+                          int(skills["expressiveness"]),
+                          int(skills["listening"]),
+                          int(skills["empathy"]),
+                          int(skills["emotional management"]) * 0.20 + 0.8 * int(skills["expressiveness"]),
+                          int(skills["expressiveness"]),
+                          int(skills["emotional management"]),
+                          int(skills["empathy"])]
+    chosen_emotion = [key for key, value in c_emotion_scores.items() if value == max(c_emotion_scores.values())]
+    chosen_emotion = chosen_emotion[0]
+    result = {
+        "chosen_emotion": chosen_emotion[0],
+        "qualifies_scores": c_qualifies_scores
+    }
+    return jsonify(result)
 
+
+if __name__ == '__main__':
     db_connection = InitMongo()
     db = DataBase(db_connection)
     config = load_config_file("configuration.json")
@@ -124,6 +169,5 @@ if __name__ == '__main__':
     simulation_id = None
 
     voice = Voice(local_config)
-    #res = IU.InitUtil.matching_customer(simulator.model_engine,db)
-
+    # res = IU.InitUtil.matching_customer(simulator.model_engine,db)
     app.run(debug=True)
--- Server/Models/templateSimulation.py
+++ Server/Models/templateSimulation.py
@@ -1,6 +1,5 @@
 
 class TemplateSimulation:
-
     def __init__(self,subject,skills):
         self.subject = subject
         self.skills = skills
